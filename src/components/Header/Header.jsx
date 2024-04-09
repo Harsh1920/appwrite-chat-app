0993ab5ba0f7244e8d733b37963d3e05362977d7
Laxy Patel <laxy@haraxy.in>
2024-04-09 11:18:04 +0530

    left panel ui wip

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router";
 import { account } from "../../lib/appwrite";
 import  logout  from "../../assets/logout.jsx";
 import Logout from "../../assets/logout.jsx";
-import { FaRocketchat } from "react-icons/fa";
+import { FaRocketchat,FaSignOutAlt } from "react-icons/fa";
 const Header = () => {
   const navigate = useNavigate();
 
@@ -19,14 +19,14 @@ const Header = () => {
 
   return (
     <div className="header">
-      <div className="header__icons">
-        <div>
-          <FaRocketchat />
-          </div>
-      <div className="header__logout" onClick={logoutHandler}>
+      <div className="header__icons_outer">
         
-        <Logout />
-        </div>
+        <FaRocketchat className="header__icon header__icon_active" />
+        
+        <FaSignOutAlt className="header__icon" onClick={logoutHandler}/>
+        {/* <div className="header__logout" onClick={logoutHandler}>        
+          <Logout />
+        </div> */}
         </div>
       
     </div>
